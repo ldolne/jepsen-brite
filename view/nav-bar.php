b4cd272e56edd70c0f44e235921e65eb93ff3c95
becode <mehdi.semlali.1988@gmail.com>
2020-07-28 12:09:49 +0200

    changement nav-bar

--- view/nav-bar.php
+++ view/nav-bar.php
@@ -1,7 +1,7 @@
 <div class="nav-scroller py-1 mb-2">
   <nav class="nav d-flex justify-content-between">
     <a class="p-2 text-muted" href="index.php">Homepage</a>
-    <a class="p-2 text-muted" href="index.php?action=event">Event</a>
+    <a class="p-2 text-muted" href="index.php?action=allcategorycontroller">Event</a>
     <a class="p-2 text-muted" href="index.php?action=oneEvent">One Event</a>
     <a class="p-2 text-muted" href="index.php?action=profil">Profile</a>
   </nav>
