08bd7c0a2cbcf5b03d3a1f75fe8f48ee0760856d
ldolne <dolnelaetitia@gmail.com>
2020-07-30 23:23:55 +0200

    Correct

--- model/Manager.php
+++ model/Manager.php
@@ -4,7 +4,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_e2d2d52caa0a60e;charset=utf8mb4', 'b0064daaa84290', 'fffd63cb', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $db = new PDO('mysql:host=localhost;dbname=jepsen-brite;charset=utf8mb4', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         return $db;
     }
 }
