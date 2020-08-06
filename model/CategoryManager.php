8665f92c308df4972a57a4edbc3ec3e8e36e1818
ldolne <dolnelaetitia@gmail.com>
2020-08-06 12:41:12 +0200

    MAJ CategoryQuery

--- model/CategoryManager.php
+++ model/CategoryManager.php
@@ -7,12 +7,17 @@ class CategoryManager extends Manager
     public function AllCategoryModel()
     {
         $bdd = $this->dbConnect();
-        $req = $bdd->query('SELECT e.id, e.title, DATE_FORMAT(e.event_date, \'%d/%m/%Y\') AS event_date_formatted, DATE_FORMAT(e.event_hour, \'%H:%i\') AS event_hour_formatted, c.category
+        $req = $bdd->query('SELECT e.id, e.title, DATE_FORMAT(e.event_date, \'%d/%m/%Y\') AS event_date_formatted, DATE_FORMAT(e.event_hour, \'%H:%i\') AS event_hour_formatted, c.category, s.subcategory
             FROM events AS e
             INNER JOIN users AS u
             ON e.author_id = u.id 
             INNER JOIN categories AS c
             ON e.category_id = c.id
+            LEFT JOIN assoc_subcategories_events AS ase
+            ON e.id = ase.event_id 
+            LEFT JOIN subcategories as s 
+            on s.id = ase.subcategory_id
+            GROUP BY e.title
             ORDER BY event_date DESC, event_hour DESC');
 
         return $req;
